5aa1be0a579fb1cbc1aba9832bd7ca69418b0853
th-1982 <>
2023-10-07 19:56:21 +0000

    Display existing tasks from the database

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -14,4 +14,8 @@ document.addEventListener("DOMContentLoaded", function () {
     // select initialization
     let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
+
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsibles);
 });
